ef281cba11bdd8246aca20845128510a66efa7fd
iMac <iMac@iMacs-iMac.local>
2020-05-06 18:27:08 -0700

    Primeros componentes creados, PhotoCards y Categories

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Listos para empezar !')
+import React from 'react'
+import ReactDom from 'react-dom'
+import {App} from './App'
+
+
+ReactDom.render(<App />, document.getElementById('app'))
+	
