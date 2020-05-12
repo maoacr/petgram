8a819ce7776403e102de2a42abf7cd4c4a39a10a
iMac <iMac@iMacs-iMac.local>
2020-05-11 23:39:45 -0700

    Observer aplicado para renderizar las PhotoCards exclusivamente cuando entren al ViewPort

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,45 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { Fragment, useState, useEffect, useRef } from 'react'
+import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
 
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+    useEffect(function() {
+        // console.log(element.current)
+         Promise.resolve(
+            typeof window.IntersectionObserver !== 'undefined'
+              ? window.IntersectionObserver
+              : import('intersection-observer')
+          ).then( () => {
+            const observer = new window.IntersectionObserver(function(entries) {
+              const { isIntersecting } = entries[0]
+              if (isIntersecting) {
+                setShow(true)
+                observer.disconnect
+              }
+            })
+            observer.observe(element.current)
+          })
+    } , [element])
+
+  return (
+    <Article ref={element} >
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </Fragment>
+      }
+    </Article>
   )
 }
