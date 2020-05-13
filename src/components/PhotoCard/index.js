812a252310d8efc5bd23f84b618165d680a53554
iMac <iMac@iMacs-iMac.local>
2020-05-12 23:16:42 -0700

    Hooks separados

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,33 +1,19 @@
-import React, { Fragment, useState, useEffect, useRef } from 'react'
+import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
-
-    useEffect(function() {
-        // console.log(element.current)
-         Promise.resolve(
-            typeof window.IntersectionObserver !== 'undefined'
-              ? window.IntersectionObserver
-              : import('intersection-observer')
-          ).then( () => {
-            const observer = new window.IntersectionObserver(function(entries) {
-              const { isIntersecting } = entries[0]
-              if (isIntersecting) {
-                setShow(true)
-                observer.disconnect
-              }
-            })
-            observer.observe(element.current)
-          })
-    } , [element])
+  const [show, element] = useNearScreen()
+  const key = `like-${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
-    <Article ref={element} >
+    <Article ref={element}>
       {
         show && <Fragment>
           <a href={`/detail/${id}`}>
@@ -35,8 +21,9 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button>
-            <MdFavoriteBorder size='32px' /> {likes} likes!
+
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size='32px' /> {likes} likes!
           </Button>
         </Fragment>
       }
