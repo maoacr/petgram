b9b133b620ece8cd82b26114fc12f784084304e8
iMac <iMac@iMacs-iMac.local>
2020-05-19 21:01:03 -0700

    implementando reach-router y probado con la page Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Fragment } from 'react'
-import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
-import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+import { Home } from './pages/Home'
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search)
@@ -16,10 +15,7 @@ export const App = () => {
       {
         detailId
           ? <PhotoCardWithQuery id={detailId} />
-          : <Fragment>
-            <ListOfCategories />
-            <ListOfPhotoCards categoryId={1} />
-          </Fragment>
+          : <Home />
       }
     </div>
   )
