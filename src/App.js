a70548a36c843e53cb5df9ab78b7c0e96464775d
iMac <iMac@iMacs-iMac.local>
2020-05-22 15:59:36 -0700

    NavBar added, Reach router added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
+import { Router } from '@reach/router'
+
+import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NavBar } from './components/NavBar'
+
+const UserLogged = ({children}) => {
+  return children({ isAuth: false })
+}
+
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
 
   return (
     <div>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Home />
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
+      
+      <UserLogged>
+          {
+            ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                  <Favs path='/favs'/>
+                  <User path='/user'/>
+                </Router>
+              : <NotRegisteredUser>
+                  <NotRegisteredUser path='/favs'/>
+                  <NotRegisteredUser path='/user'/>
+                </NotRegisteredUser> 
+          }
+      </UserLogged>
+
+
+      <NavBar />
     </div>
   )
 }
